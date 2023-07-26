60cb23fd1d2cf0b6b3c8ef8d5abe1e0b55f712df
asillers <asillers@ucdavis.edu>
2023-07-26 11:55:34 -0700

    scripts update, genelength scripts not yet working

---
+++ genelengths.R
@@ -0,0 +1,27 @@
+#parts of code adapted from https://github.com/dpryan79/Answers/blob/master/SEQanswers_42420/GTF2LengthGC.R
+
+library(GenomicRanges)
+library(rtracklayer)
+library(Rsamtools)
+
+#read in gtf file
+
+path <- "/home/asillers/Genome/farr1.gene_models.gtf"
+GTF <- import(path, format="gtf", genome="FaRR1", feature.type="exon")
+
+#reduce
+
+rGTF <- reduce(split(GTF, elementMetadata(GTF)$gene_id))
+reducedGTF <- unlist(rGTF, use.names=TRUE)
+elementMetadata(reducedGTF)$gene_id <- rep(names(rGTF), elementNROWS(rGTF))
+
+#make table
+
+data <- split(reducedGTF, elementMetadata(reducedGTF)$gene_id)
+data <- do.call(cbind, data)
+output <- as.matrix(t(data))
+colnames(output) <- c("Length")
+
+#write output
+
+write.table(output, "genelengths.tsv", sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
---
+++ genelengths.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --job-name=genelengths
+#SBATCH --ntasks=10 # Number of cores
+#SBATCH --nodes=1 # Ensure that all cores are on one machine
+#SBATCH --mem=20G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --partition=bmh # Partition to submit to
+#SBATCH --output=genelengths.out # File to which STDOUT will be written
+#SBATCH --error=genelengths.err # File to which STDERR will be written
+#SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
+#SBATCH --time=08:00:00
+
+set -e                                                                                  # Error if a single command fails
+set -u                                                                                  # Error if un-named variables calledset -x  >
+set -x
+
+module load conda/latest
+conda activate expression
+
+gtftools -l rrgenelengths.txt /home/asillers/Genome/farr1.gene_models.gtf.ensembl
