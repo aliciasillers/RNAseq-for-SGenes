9840860996b275ec527a93f5b899d1109284c357
asillers <asillers@ucdavis.edu>
2023-07-28 15:26:27 -0700

    add rpkm scripts, update exp2.yml

---
+++ calcexp.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(DBI)
+
+#read in files
+
+countpath <- "Counted/allcounts.copy.tsv"
+genelengthpath <- "rrgenelengths.tsv"
+
+counts <- read.table(countpath, header=TRUE, sep="\t")
+counts <- as.data.frame(counts)
+counts <- sqlColumnToRownames(counts, row.names="gene")
+
+genelengths <- read.delim(genelengthpath, header=TRUE, sep="\t")
+genelength.mean <- as.vector(genelengths[,2])
+
+#calculate reads per kilobase per million (rpkm)
+
+rpkm.fun <- function(column) {
+	
+	10^9 * column/genelength.mean/sum(column)
+}
+exp.rpkm <- data.frame(sapply(counts, rpkm.fun))
+
+#write output
+
+counts <- sqlRownamesToColumn(counts, row.names = "gene")
+output <- cbind(counts$gene, exp.rpkm)
+
+write.table(output, "rpkm.tsv", sep="\t", quote=FALSE, row.names=FALSE)
---
+++ calcexp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --job-name=calcexp
+#SBATCH --ntasks=10 # Number of cores
+#SBATCH --nodes=1 # Ensure that all cores are on one machine
+#SBATCH --mem=22G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --partition=bmh # Partition to submit to
+#SBATCH --output=calcexp.out # File to which STDOUT will be written
+#SBATCH --error=calcexp.err # File to which STDERR will be written
+#SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
+#SBATCH --time=1-00:00:00
+
+set -e                                                                                  # Error if a single command fails
+set -u                                                                                  # Error if un-named variables calledset -x  >
+set -x
+
+module load conda/latest
+conda activate exp2
+
+Rscript --vanilla --verbose calcexp.R
